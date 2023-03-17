379b0e63f98261319c973eaff515d13e9b171108
Sportkanone123 <kim-mueller@gmx.com>
2023-03-17 14:48:22 +0100

    Fixes issue with simple_location_page.dart.

--- lib/ui/templates/simple_location_page.dart
+++ lib/ui/templates/simple_location_page.dart
@@ -14,7 +14,7 @@ class SimpleLocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 144,
+      height: 122,
       margin: const EdgeInsets.only(bottom: 20),
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(11)),
@@ -25,25 +25,30 @@ class SimpleLocationPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
                 width: 120,
-                margin: const EdgeInsets.only(top: 25, left: 20),
-                child: AutoSizeText("${simpleWeatherProfile.location.name.capitalize()}, ${simpleWeatherProfile.location.country}", maxLines: 1, maxFontSize: 20, textAlign: TextAlign.center, style: const TextStyle(color: kTextSecondaryColor, fontSize: 20, fontWeight: FontWeight.w600, fontFamily: "Poppins")),
+                margin: const EdgeInsets.only(left: 20),
+                child: AutoSizeText("${simpleWeatherProfile.location.name.capitalize()}, ${simpleWeatherProfile.location.country}", maxLines: 1, maxFontSize: 25, textAlign: TextAlign.center, style: const TextStyle(color: kTextSecondaryColor, fontSize: 20, fontWeight: FontWeight.w600, fontFamily: "Poppins")),
               ),
               Container(
-                margin: const EdgeInsets.only(bottom: 25, left: 20),
+                margin: const EdgeInsets.only(left: 20),
                 child: Text("${simpleWeatherProfile.temperature.round()}°", textAlign: TextAlign.center, style: const TextStyle(color: kTextSecondaryColor, fontSize: 60, fontWeight: FontWeight.w500, fontFamily: "Poppins")),
               ),
             ],
           ),
-          Container(
-            height: 113,
-            width: 89,
-            margin: const EdgeInsets.only(bottom: 25,  right: 30),
-            child: IconProvider().getSmallWeatherIcon(simpleWeatherProfile.weatherId),
-          ),
+          Expanded(
+              child: Container(
+                height: 113,
+                width: 89,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(right: 30),
+                child: IconProvider().getSmallWeatherIcon(simpleWeatherProfile.weatherId),
+              ),
+          )
+
         ],
       ),
     );
--- lib/utils/icon_provider.dart
+++ lib/utils/icon_provider.dart
@@ -100,12 +100,12 @@ class IconProvider {
       case 230:
       case 231:
       case 232:
-        return SvgPicture.asset('assets/weather_icons/thunderstorm_rain.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/thunderstorm_rain.svg', width: 89);
       case 210:
       case 211:
       case 212:
       case 221:
-        return SvgPicture.asset('assets/weather_icons/thunderstorm.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/thunderstorm.svg', width: 89);
       case 300:
       case 301:
       case 302:
@@ -115,38 +115,38 @@ class IconProvider {
       case 313:
       case 314:
       case 321:
-        return SvgPicture.asset('assets/weather_icons/drizzle.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/drizzle.svg', width: 89);
       case 500:
       case 501:
       case 502:
       case 503:
       case 504:
         if(_isDay()) {
-          return SvgPicture.asset('assets/weather_icons/rain_day.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/rain_day.svg', width: 89);
         }else{
-          return SvgPicture.asset('assets/weather_icons/rain_night.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/rain_night.svg', width: 89);
         }
       case 511:
-        return SvgPicture.asset('assets/weather_icons/freezing_rain.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/freezing_rain.svg', width: 89);
       case 520:
       case 521:
       case 522:
       case 531:
-        return SvgPicture.asset('assets/weather_icons/heavy_rain.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/heavy_rain.svg', width: 89);
       case 600:
       case 601:
       case 602:
-        return SvgPicture.asset('assets/weather_icons/snow.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/snow.svg', width: 89);
       case 611:
       case 612:
       case 613:
       case 615:
       case 616:
-        return SvgPicture.asset('assets/weather_icons/freezing_rain.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/freezing_rain.svg', width: 89);
       case 620:
       case 621:
       case 622:
-        return SvgPicture.asset('assets/weather_icons/shower_snow.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/shower_snow.svg', width: 89);
       case 701:
       case 711:
       case 721:
@@ -155,28 +155,28 @@ class IconProvider {
       case 751:
       case 761:
       case 762:
-        return SvgPicture.asset('assets/weather_icons/foggy.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/foggy.svg', width: 89, fit: BoxFit.fill,);
       case 771:
       case 781:
-        return SvgPicture.asset('assets/weather_icons/storm.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/storm.svg', width: 89, fit: BoxFit.fill,);
       case 800:
         if(_isDay()) {
-          return SvgPicture.asset('assets/weather_icons/clear_day.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/clear_day.svg', width: 89, fit: BoxFit.fill,);
         }else{
-          return SvgPicture.asset('assets/weather_icons/clear_night.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/clear_night.svg', width: 89, fit: BoxFit.fill,);
         }
       case 801:
         if(_isDay()) {
-          return SvgPicture.asset('assets/weather_icons/few_clouds_day.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/few_clouds_day.svg', width: 89, fit: BoxFit.fill,);
         }else{
-          return SvgPicture.asset('assets/weather_icons/few_clouds_night.svg', fit: BoxFit.fitWidth,);
+          return SvgPicture.asset('assets/weather_icons/few_clouds_night.svg', width: 89, fit: BoxFit.fill,);
         }
       case 802:
       case 803:
       case 804:
-        return SvgPicture.asset('assets/weather_icons/clouds.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/clouds.svg', width: 89, fit: BoxFit.fill,);
       default:
-        return SvgPicture.asset('assets/weather_icons/fetch_failed.svg', fit: BoxFit.fitWidth,);
+        return SvgPicture.asset('assets/weather_icons/fetch_failed.svg', width: 89, fit: BoxFit.fill,);
     }
   }
 
